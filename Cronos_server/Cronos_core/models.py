7bf106ed6362a3cb07600efa920d8da6c657c24c
ValentinePumpkins <quignon.val@gmail.com>
2024-01-16 17:26:38 +0100

    create class Profiles, Crons + Logs

--- Cronos_server/Cronos_core/models.py
+++ Cronos_server/Cronos_core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profiles(models.Model):
+    first_name = models.CharField(max_length=64, null=True)
+    last_name = models.CharField(max_length=64, null=True)
+    user = models.OneToOneField(
+        User, null=True, blank=True, on_delete=models.SET_NULL)
+
+
+class Crons(models.Model):
+    cron = models.CharField(max_length=64)
+    validated = models.BooleanField()
+    is_paused = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    log = models.ForeignKey('Logs', on_delete=models.CASCADE)
+
+
+class Logs(models.Model):
+    log = models.CharField(max_length=64)
+    create_date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cron = models.ForeignKey('Crons', on_delete=models.CASCADE)
