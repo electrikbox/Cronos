bc2310c9541f8d24e55806645345b070c6b79549
ValentinePumpkins <quignon.val@gmail.com>
2024-01-16 13:42:01 +0100

    add mysql config

--- Cronos_server/Cronos_server/settings.py
+++ Cronos_server/Cronos_server/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Cronos_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cronos',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
