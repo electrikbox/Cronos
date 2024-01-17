5d01e78dbf0a1c4bca53c699d9b3b71821eb07cd
ValentinePumpkins <quignon.val@gmail.com>
2024-01-17 17:43:28 +0100

    update + delete views

--- Cronos_server/Cronos_API/views.py
+++ Cronos_server/Cronos_API/views.py
@@ -19,6 +19,7 @@ def list_crons(request):
     serializer = CronsSerializer(crons, many=True)
     return Response(serializer.data)
 
+
 # CRONS Create
 # ======================================================
 
@@ -31,7 +32,7 @@ def add_cron(request) -> Response:
         cron_instance = cron_serializer.save()
 
         log_data = {
-            "log": "OK",
+            "log": "Creation OK",
             "create_date": datetime.now(),
             "user": request.data.get('user'),
             "cron": cron_instance.id,
@@ -58,3 +59,44 @@ def show_cron(request, cron_id):
     cron_instance = get_object_or_404(models.Crons, pk=cron_id)
     serializer = CronsSerializer(cron_instance)
     return Response(serializer.data)
+
+
+# CRONS Update one cron
+# ======================================================
+@api_view(['PUT'])
+def update_cron(request, cron_id):
+    """ Update a cron by its id """
+    cron_instance = get_object_or_404(models.Crons, pk=cron_id)
+    cron_serializer = CronsSerializer(
+        cron_instance, data=request.data, partial=True)
+
+    if cron_serializer.is_valid():
+        cron_serializer.save(updated_date=datetime.now())
+
+        log_data = {
+            "log": "Update OK",
+            "create_date": datetime.now(),
+            "user": cron_instance.user.id if cron_instance.user else None,
+            "cron": cron_instance.id,
+        }
+
+        log_serializer = LogsSerializer(data=log_data)
+
+        if log_serializer.is_valid():
+            log_serializer.save()
+            return Response(cron_serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(log_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response(cron_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# CRONS Delete
+# ======================================================
+
+@api_view(['DELETE'])
+def delete_cron(request, cron_id):
+    """ Delete a cron by its id """
+    cron_instance = get_object_or_404(models.Crons, pk=cron_id)
+    cron_instance.delete()
+    return Response({"message": "Cron deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
