606978805ec0c04383004c178622ee099569b633
ValentinePumpkins <quignon.val@gmail.com>
2024-01-17 16:05:57 +0100

    add path crons/id to show 1 cron

--- Cronos_server/Cronos_API/urls.py
+++ Cronos_server/Cronos_API/urls.py
@@ -1,11 +1,13 @@
 """ URLs for Cronos_API """
 
 from django.urls import path
-from .views import list_crons, add_cron
+from .views import list_crons, add_cron, show_cron
+
 
 app_name = 'Cronos_API'
 
 urlpatterns = [
     path('crons/', list_crons, name='crons'),
-    path('crons/create/', add_cron, name='add_cron')
+    path('crons/create/', add_cron, name='add_cron'),
+    path('crons/<cron_id>/', show_cron, name='show_cron')
 ]
