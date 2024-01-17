269cdb6b84e25f46a4536b7d14b59bf659165b6f
ValentinePumpkins <quignon.val@gmail.com>
2024-01-17 14:51:58 +0100

    add create path

--- Cronos_server/Cronos_API/urls.py
+++ Cronos_server/Cronos_API/urls.py
@@ -1,6 +1,11 @@
+""" URLs for Cronos_API """
+
 from django.urls import path
-from .views import list_crons
+from .views import list_crons, add_cron
+
+app_name = 'Cronos_API'
 
 urlpatterns = [
     path('crons/', list_crons, name='crons'),
+    path('crons/create/', add_cron, name='add_cron')
 ]
