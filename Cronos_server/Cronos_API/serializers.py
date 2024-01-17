762f5a34cfb35e4a46b61d091cf69f5a10fcacc4
ValentinePumpkins <quignon.val@gmail.com>
2024-01-17 14:51:04 +0100

    add logSerializer + updated ConSerializer
    Co-authored-by: Electrik <electrikbox@users.noreply.github.com>

--- Cronos_server/Cronos_API/serializers.py
+++ Cronos_server/Cronos_API/serializers.py
@@ -1,8 +1,24 @@
-from rest_framework import serializers
-from Cronos_core.models import Crons
+""" Serializers for Cronos_API """
 
+from rest_framework import serializers
+from Cronos_core.models import Logs, Crons
+
+
+# LOG
+# ===================================================
+
+class LogsSerializer(serializers.ModelSerializer):
+    """ Serializer for Logs """
+    class Meta:
+        model = Logs
+        fields = ["log", "create_date", "user", "cron"]
+
+
+# CRONS
+# ====================================================
 
 class CronsSerializer(serializers.ModelSerializer):
+    """ Serializer for Crons """
     class Meta:
         model = Crons
-        fields = ['cron', 'is_paused']
+        fields = ["cron", "user", "is_paused", "validated"]
