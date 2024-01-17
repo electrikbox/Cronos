75a583390e414de7a687e507ca8b146c8ea763fd
ValentinePumpkins <quignon.val@gmail.com>
2024-01-17 17:42:44 +0100

    add update + delete path

--- Cronos_server/Cronos_API/urls.py
+++ Cronos_server/Cronos_API/urls.py
@@ -1,7 +1,7 @@
 """ URLs for Cronos_API """
 
 from django.urls import path
-from .views import list_crons, add_cron, show_cron
+from .views import list_crons, add_cron, show_cron, update_cron, delete_cron
 
 
 app_name = 'Cronos_API'
@@ -9,5 +9,7 @@ app_name = 'Cronos_API'
 urlpatterns = [
     path('crons/', list_crons, name='crons'),
     path('crons/create/', add_cron, name='add_cron'),
-    path('crons/<cron_id>/', show_cron, name='show_cron')
+    path('crons/<cron_id>/', show_cron, name='show_cron'),
+    path('crons/<cron_id>/update/', update_cron, name='update_cron'),
+    path('crons/<cron_id>/delete/', delete_cron, name='delete_cron'),
 ]
