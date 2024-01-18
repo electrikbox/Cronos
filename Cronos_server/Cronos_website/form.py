47f889b1d5918b6966f201de58e21d89f31c6849
electrikbox <electrikbox@icloud.com>
2024-01-18 18:24:05 +0100

    make the class more simple to start

--- Cronos_server/Cronos_website/form.py
+++ Cronos_server/Cronos_website/form.py
@@ -2,13 +2,8 @@ from django import forms
 from Cronos_core.models import Crons
 
 class CronForm(forms.ModelForm):
+    minutes = forms.ChoiceField(choices=Crons.get_minutes_choices())
+
     class Meta:
         model = Crons
-        fields = ["cron"]
-        minute = forms.IntegerField(min_value=0, max_value=59)
-        hour = forms.IntegerField(min_value=0, max_value=23)
-        day_of_month = forms.IntegerField(min_value=1, max_value=31)
-        month = forms.IntegerField(min_value=1, max_value=12)
-        day_of_week = forms.IntegerField(min_value=0, max_value=6)
-        command = forms.CharField(max_length=64)
-        widgets = {}
+        fields = ['user']
