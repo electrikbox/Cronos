e4e40126652b5c72abe8023e9592696b2f4c181d
ValentinePumpkins <quignon.val@gmail.com>
2024-01-19 16:19:57 +0100

    add class method

--- Cronos_server/Cronos_core/models.py
+++ Cronos_server/Cronos_core/models.py
@@ -29,9 +29,9 @@ class Crons(models.Model):
     """ Class Crons """
     minutes = models.CharField(max_length=64)
     hours = models.CharField(max_length=64)
+    day_of_month = models.CharField(max_length=64)
     months = models.CharField(max_length=64)
     day_of_week = models.CharField(max_length=64)
-    day_of_month = models.CharField(max_length=64)
     command = models.CharField(max_length=64)
 
     create_date = models.DateTimeField(default=timezone.now)
@@ -42,6 +42,59 @@ class Crons(models.Model):
 
     @classmethod
     def get_minutes_choices(cls):
-        choices = [(str(i), str(i).zfill(2)) for i in range(0, 60)]
-        choices.extend((f'*/{i}', f'every {i}') for i in range(5, 60, 5))
+        """ Minutes choices for cron creation """
+        choices = [(str(minutes), str(minutes).zfill(2))
+                   for minutes in range(0, 60)]
+        choices.extend((f'*/{minutes}', f'every {minutes}')
+                       for minutes in range(5, 60, 5))
+        choices.append(('*', 'every minutes'))
+        return choices
+
+    @classmethod
+    def get_hours_choices(cls):
+        """ Hours choices for cron creation """
+        choices = [(str(hours), str(hours).zfill(2)) for hours in range(0, 24)]
+        choices.extend((f'*/{hours}', f'every {hours}')
+                       for hours in range(5, 24, 5))
+        choices.append(('*', 'every hours'))
+        return choices
+
+    @classmethod
+    def get_day_of_month_choices(cls):
+        """ Day of the month choice for cron creation """
+        choices = [(str(day), str(day).zfill(2)) for day in range(1, 32)]
+        choices.extend((f'*/{day}', f'every {day}') for day in range(5, 32, 5))
+        choices.append(('*', 'every day of month'))
+        return choices
+
+    @classmethod
+    def get_months_choices(cls):
+        """ Months choices for cron creation """
+        months_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May',
+                        'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        choices = [(str(month), month) for month in months_names]
+        choices.extend((f'*/{month}', f'every {month}')
+                       for month in months_names)
+        choices.append(('*', 'every month'))
+        return choices
+
+    @classmethod
+    def get_day_of_week_choices(cls):
+        """ Day of the week choice for cron creation """
+        days_of_week = ['Monday', 'Tuesday', 'Wednesday',
+                        'Thursday', 'Friday', 'Saturday', 'Sunday']
+        choices = [(day[:3], day[:3].capitalize()) for day in days_of_week]
+        choices.extend((f'*/{day[:3]}', f'every {day[:3]}')
+                       for day in days_of_week)
+        choices.append(('*', 'every day of the week'))
+        return choices
+
+    @classmethod
+    def get_command_choices(cls):
+        """ Command choices for cron creation """
+        choices = [
+            ('echo "Hello World"', 'echo "Hello World"'),
+            ('ls', 'List Files (ls)'),
+            ('pwd', 'Print Working Directory (pwd)'),
+        ]
         return choices
