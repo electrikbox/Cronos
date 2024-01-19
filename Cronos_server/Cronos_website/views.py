d0dc44f77d48b31a11553bc05527721a0c48d0b4
ValentinePumpkins <quignon.val@gmail.com>
2024-01-19 16:20:27 +0100

    form is valid added

--- Cronos_server/Cronos_website/views.py
+++ Cronos_server/Cronos_website/views.py
@@ -6,10 +6,13 @@ from .form import CronForm
 def form(request):
     if request.method == "POST":
         form = CronForm(request.POST)
-        return HttpResponse(form.data['minutes'])
+        if form.is_valid():
+            return HttpResponse([form.data[key] for key in ['minutes', 'hours', 'day_of_month', 'months', 'day_of_week', 'command']])
+        else:
+            return HttpResponse("Error Form")
     else:
         form = CronForm()
-    
+
     context = {"form": form}
 
     return render(request, "form.html", context)
