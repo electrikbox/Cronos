d22839c32a3262ff8d9bc7c3d994d473c211ff79
electrikbox <electrikbox@icloud.com>
2024-01-19 14:05:52 +0100

    validate all fields to add in db for api requests

--- Cronos_server/Cronos_API/serializers.py
+++ Cronos_server/Cronos_API/serializers.py
@@ -3,48 +3,120 @@
 from rest_framework import serializers
 from Cronos_core.models import Logs, Crons
 from django.contrib.auth.models import User
+from Cronos_API import *
+
+
+# USER
+# =============================================================================
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta(object):
+        model = User
+        fields = ["id", "username", "password", "email"]
 
 
 # LOG
-# ===================================================
+# =============================================================================
+
 
 class LogsSerializer(serializers.ModelSerializer):
-    """ Serializer for Logs """
+    """Serializer for Logs"""
+
     class Meta:
         model = Logs
         fields = ["log", "create_date", "user", "cron"]
 
 
 # CRONS
-# ====================================================
+# =============================================================================
+
 
 class CronsSerializer(serializers.ModelSerializer):
-    """ Serializer for Crons """
+    """Serializer for Crons"""
+
     class Meta:
         model = Crons
         fields = [
             "minutes",
             "hours",
-            "month"
-            "day_of_week",
             "day_of_month",
+            "months",
+            "day_of_week",
             "command",
             "user",
             "is_paused",
-            "validated"
+            "validated",
         ]
-        
-    def validate_command(self, value: str) -> str:
-        if value.split(" ")[0] == "rm":
-            raise serializers.ValidationError("command not allowed")
+
+    # validate minutes (0 to 59 or  */5 to */55 or *)
+    # =========================================================================
+
+    def validate_minutes(self, value: str) -> str:
+        if value not in(MINUTES_RANGE + EVERY_MINUTES_RANGE + ["*"]):
+            raise serializers.ValidationError(MINUTES_ALLOWED_MSG)
+        else:
+            return value
+
+    # validate hours  (0 to 23 or  */1 to */23 or *)
+    # =========================================================================
+
+    def validate_hours(self, value: str) -> str:
+        if value not in(HOURS_RANGE + EVERY_HOURS_RANGE + ["*"]):
+            raise serializers.ValidationError(HOURS_ALLOWED_MSG)
         else:
             return value
 
 
-# USER
-# ====================================================
+    # validate day of month  (1 to 31 or  */1 to */31 or *)
+    # =========================================================================
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta(object):
-        model = User
-        fields = ['id', 'username', 'password', 'email']
+    def validate_day_of_month(self, value: str) -> str:
+        if value not in(DAY_OF_MONTH_RANGE + EVERY_DAY_OF_MONTH_RANGE + ["*"]):
+            raise serializers.ValidationError(DAY_OF_MONTH_ALLOWED_MSG)
+
+        elif (
+            value in (DAY_OF_MONTH_RANGE + EVERY_DAY_OF_MONTH_RANGE)
+            and (self.initial_data["day_of_week"] != "*")
+        ):
+            raise serializers.ValidationError(
+                DAY_OF_MONTH_ERROR_MSG.format(day_of_month=self.initial_data["day_of_month"]))
+        else:
+            return value
+
+
+    # validate months  (1 to 12 or  */1 to */12 or *)
+    # =========================================================================
+
+    def validate_months(self, value: str) -> str:
+        if value not in (MONTHS_RANGE + EVERY_MONTHS_RANGE+ ["*"]):
+            raise serializers.ValidationError(MONTHS_ALLOWED_MSG)
+        else:
+            return value
+
+
+    # validate day of week  (mon, tue, wed, thu, fri, sat, sun or  */1 to */7 or *)
+    # =========================================================================
+
+    def validate_day_of_week(self, value: str) -> str:
+        if value not in (DAY_OF_WEEK_RANGE + EVERY_DAY_OF_WEEK_RANGE + ["*"]):
+            raise serializers.ValidationError(DAY_OF_WEEK_ALLOWED_MSG)
+
+        elif (
+            value in (DAY_OF_WEEK_RANGE + EVERY_DAY_OF_WEEK_RANGE)
+            and (self.initial_data["day_of_month"] != "*")
+        ):
+            raise serializers.ValidationError(
+                DAY_OF_WEEK_ERROR_MSG.format(day_of_week=self.initial_data["day_of_week"]))
+        else:
+            return value
+
+
+    # validate commands (open, ls)
+    # =========================================================================
+
+    def validate_command(self, value: str) -> str:
+        if not value.split(" ")[0] in ALLOWED_COMMANDS:
+            raise serializers.ValidationError(COMMANDS_ALLOWED_MSG)
+        else:
+            return value
