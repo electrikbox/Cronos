b4525af122d51ce22c51d1d5801e2c3c7e8dbb87
electrikbox <electrikbox@icloud.com>
2024-01-20 11:50:23 +0100

    add labels

--- Cronos_server/Cronos_website/form.py
+++ Cronos_server/Cronos_website/form.py
@@ -4,12 +4,12 @@ from Cronos_core.models import Crons
 
 class CronForm(forms.ModelForm):
     """Crons form class"""
-    minutes = forms.ChoiceField(choices=Crons.get_minutes_choices())
-    hours = forms.ChoiceField(choices=Crons.get_hours_choices())
-    day_of_month = forms.ChoiceField(choices=Crons.get_day_of_month_choices())
-    months = forms.ChoiceField(choices=Crons.get_months_choices())
-    day_of_week = forms.ChoiceField(choices=Crons.get_day_of_week_choices())
-    command = forms.ChoiceField(choices=Crons.get_command_choices())
+    minutes = forms.ChoiceField(choices=Crons.get_minutes_choices(), label='Minutes')
+    hours = forms.ChoiceField(choices=Crons.get_hours_choices(), label='Hours')
+    day_of_month = forms.ChoiceField(choices=Crons.get_day_of_month_choices(), label="Month's day")
+    months = forms.ChoiceField(choices=Crons.get_months_choices(), label='Month')
+    day_of_week = forms.ChoiceField(choices=Crons.get_day_of_week_choices(), label="Week's day")
+    command = forms.ChoiceField(choices=Crons.get_command_choices(), label='Command')
 
     class Meta:
         model = Crons
