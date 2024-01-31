2eb7233571b5467ce2bbabb76c6fb386c423c6d9
ValentinePumpkins <quignon.val@gmail.com>
2024-01-31 17:54:52 +0100

    add class signupform

--- Cronos_server/Cronos_website/forms.py
+++ Cronos_server/Cronos_website/forms.py
@@ -1,6 +1,8 @@
 """ Class CronFom """
 from django import forms
 from Cronos_core.models import Crons
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class CronForm(forms.ModelForm):
@@ -11,18 +13,18 @@ class CronForm(forms.ModelForm):
         initial=00
     )
     hours = forms.ChoiceField(
-        choices=Crons.get_hours_choices(), 
-        label='Hours', 
+        choices=Crons.get_hours_choices(),
+        label='Hours',
         initial=10
     )
     day_of_month = forms.ChoiceField(
-        choices=Crons.get_day_of_month_choices(), 
-        label="Month's day", 
+        choices=Crons.get_day_of_month_choices(),
+        label="Month's day",
         initial='*'
     )
     months = forms.ChoiceField(
-        choices=Crons.get_months_choices(), 
-        label='Month', 
+        choices=Crons.get_months_choices(),
+        label='Month',
         initial='*'
     )
     day_of_week = forms.ChoiceField(
@@ -40,3 +42,33 @@ class CronForm(forms.ModelForm):
         model = Crons
         fields = ['minutes', 'hours', 'day_of_month',
                   'months', 'day_of_week', 'command']
+
+
+# CLASS SIGNUPFORM
+# =============================================================================
+
+class SignUpForm(UserCreationForm):
+    """ Sign up form class """
+    username = forms.CharField(
+        label="Username",
+        max_length=15,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your username'}),
+    )
+    email = forms.EmailField(
+        label="Email",
+        max_length=50,
+        widget=forms.EmailInput(attrs={'placeholder': 'my-email@mail.com'}),
+    )
+
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={'placeholder': 'Choose a complex password'}),
+    )
+    password2 = forms.CharField(
+        label="Confirm your password",
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm your password'}),
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
