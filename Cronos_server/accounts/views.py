8ac2894d080939a11f477a1dca8c37f53d3af17c
ValentinePumpkins <quignon.val@gmail.com>
2024-02-02 17:17:50 +0100

    change login to implement custom login form

--- Cronos_server/accounts/views.py
+++ Cronos_server/accounts/views.py
@@ -1,39 +1,20 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib.auth.forms import AuthenticationForm
 from Cronos_website.forms import SignUpForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from Cronos_core.models import Profiles
 from django.contrib.auth.models import User
+from Cronos_website.forms import LoginFormCustom
 
 
-def login_user(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(request.GET.get('next', '/'))
-        else:
-            messages.error(request, "Invalid username or password.")
-            return HttpResponseRedirect(request.GET.get('next', '/'))
-    else:
-        form = AuthenticationForm()
-        return render(request, 'accounts/login.html', {'login_form': form})
-
-
-def logout_user(request):
-    logout(request)
-    return redirect('accounts:login')
-
+# SIGNUP USER
+# =============================================================================
 
 def signup_user(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
+
         if form.is_valid():
             username = form.cleaned_data.get('username')
             first_name = form.cleaned_data.get('first_name')
@@ -47,6 +28,7 @@ def signup_user(request):
             user.save()
 
             profile = Profiles.objects.create(user=user)
+
             if first_name and last_name:
                 profile.first_name = first_name
                 profile.last_name = last_name
@@ -59,3 +41,33 @@ def signup_user(request):
     else:
         form = SignUpForm()
         return render(request, 'accounts/signup.html', {'signup_form': form})
+
+
+# LOGIN USER
+# =============================================================================
+
+def login_user(request):
+    if request.method == 'POST':
+        form = LoginFormCustom(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(request.GET.get('next', '/'))
+            else:
+                messages.error(request, "Invalid username or password.")
+                return HttpResponseRedirect(request.GET.get('next', '/'))
+    else:
+        form = LoginFormCustom()
+        return render(request, 'accounts/login.html', {'login_form': form})
+
+# LOGOUT USER
+# =============================================================================
+
+def logout_user(request):
+    logout(request)
+    return redirect('accounts:login')
