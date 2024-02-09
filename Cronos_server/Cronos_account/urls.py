3a6fcd4c944352a437ad69a781db129a9df2a878
ValentinePumpkins <quignon.val@gmail.com>
2024-02-09 14:16:41 +0100

    urls for pending/activation account

--- Cronos_server/Cronos_account/urls.py
+++ Cronos_server/Cronos_account/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('account/', views.account, name='account'),
     path('change_password/', views.change_password, name='change_password'),
     path('forget_password/', views.forget_password, name='forget_password'),
+    path('signup/pending/', views.pending_activation, name='pending_activation'),
+    path('activate/<uidb64>/<token_key>/', views.activate_account, name='activate_account'),
 ]
