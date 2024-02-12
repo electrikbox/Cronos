498165f958678a1326fac2082449e000eae83abc
ValentinePumpkins <quignon.val@gmail.com>
2024-02-12 16:42:57 +0100

    views for forget + reset pwd

--- Cronos_server/Cronos_account/views.py
+++ Cronos_server/Cronos_account/views.py
@@ -1,7 +1,6 @@
-from Cronos_server.mail import activation_mail
-from Cronos_website.forms import SignUpForm
-from Cronos_core.models import Profiles
-from Cronos_website.forms import LoginFormCustom
+from Cronos_server.mail import activation_mail, forget_password_mail
+from Cronos_core.models import PasswordTemporaryToken, Profiles
+from Cronos_website.forms import SignUpForm, LoginFormCustom, ForgetPasswordForm, SetNewPasswordForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import HttpResponseRedirect
@@ -112,7 +111,7 @@ def login_user(request):
         if user is None:
             messages.error(request, "Invalid username or password.")
             return render(request, 'accounts/login.html', {'login_form': form})
-        
+
         if user.is_active:
             login(request, user)
             return HttpResponseRedirect(request.GET.get('next', '/'))
@@ -155,4 +154,58 @@ def change_password(request):
 
 def forget_password(request):
     """ Render forget password page """
-    return render(request, 'accounts/forget_password.html')
+    if request.method == 'POST':
+        form = ForgetPasswordForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+
+            try:
+               user = User.objects.get(email=email)
+               username = user.username
+
+               token_temp = PasswordTemporaryToken.objects.create(user=user)
+               token_temp.save()
+               uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+               token_temp_key = token_temp.change_pwd_token
+
+               forget_password_mail(email, username, uidb64, token_temp_key)
+
+               return HttpResponseRedirect(reverse('Cronos_account:forget_password') + '?pending=true')
+
+            except User.DoesNotExist:
+                messages.error(request, "User with this email doesn't exist.")
+                return render(request, 'accounts/forget_password.html', {'forget_password_form': form})
+    else:
+        form = ForgetPasswordForm()
+        return render(request, 'accounts/forget_password.html', {'forget_password_form': form})
+
+
+# RESET FORGOTTEN PASSWORD
+# =============================================================================
+def reset_password(request, uidb64, token_temp_key):
+    """ Render reset forgotten password page """
+    try:
+        uid = (urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+        token_temp = PasswordTemporaryToken.objects.get(user=user, change_pwd_token=token_temp_key)
+
+        form = SetNewPasswordForm(request.POST)
+
+        if token_temp.is_valid():
+            if request.method == 'POST':
+                if form.is_valid():
+                    user.set_password(form.cleaned_data['new_password'])
+                    user.save()
+
+                    token_temp.delete()
+                    return HttpResponseRedirect(reverse('Cronos_account:login') + '?reset=true')
+            else:
+                return render(request, 'accounts/reset_password.html', {'change_pwd_form': form})
+
+        messages.error(request, "The password reset link has expired. Please request a new one")
+        return redirect('Cronos_account:forget_password')
+
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist, PasswordTemporaryToken.DoesNotExist):
+        messages.error(request, "Invalid password reset link")
+        return redirect('Cronos_account:login')
