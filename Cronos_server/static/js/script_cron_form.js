c6b43051edffdafebcdda6741cd6231c75efc165
electrikbox <electrikbox@icloud.com>
2024-02-13 12:07:04 +0100

    handle front css for pause

--- Cronos_server/static/js/script_cron_form.js
+++ Cronos_server/static/js/script_cron_form.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Gestion du changement des sélecteurs
-  $('#id_day_of_week, #id_day_of_month').change(function() {
+  $('#id_day_of_week, #id_day_of_month').change(function () {
     const selectedValue = $(this).val();
     const dayOfWeek = $('#id_day_of_week');
     const dayOfMonth = $('#id_day_of_month');
@@ -11,40 +11,51 @@ $(document).ready(function() {
     }
   });
 
-  $('.delete-cron').click(function() {
+  $('.delete-cron').click(function () {
     const cronId = $(this).data('cron-id');
     const confirmation = confirm('Are you sure you want to delete this cron?');
+    const row = $(this).closest('.cron');
 
     if (confirmation) {
-      $.post(`/api/crons/${cronId}/delete/`, {
-        csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
-      })
-      .done(function(response) {
-        location.reload();
-      })
-      .catch(function(xhr) {
-        console.log(`${xhr.status}: ${xhr.responseText}`);
-        alert('Failed to delete cron');
+      $.ajax({
+        url: `/api/crons/${cronId}/delete/`,
+        type: 'POST',
+        data: {
+          csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+        },
+        success: function (response) {
+          row.remove();
+        },
+        error: function (xhr) {
+          console.log(`${xhr.status}: ${xhr.responseText}`);
+          alert('Failed to delete cron');
+        }
       });
     }
   });
 
-  // Gestion de la soumission du formulaire
-  // $('#cron-create-form').submit(function(e) {
-  //   e.preventDefault(); // Empêche l'envoi par défaut du formulaire
+  $('.pause-cron').click(function () {
+    // Select p from nearest .cron class
+    var paragraph = $(this).closest('.cron').find('p');
 
-  //   $.post('http://localhost:8000/dashboard/', $(this).serialize())
-  //     .done(function(response) {
-  //       $('#cron-list-container').html(response);
-  //       $('#cron-create-form')[0].reset();
-  //     })
-  //     .catch(function(xhr) {
-  //       console.log(xhr.status + ": " + xhr.responseText);
-  //       var errors = xhr.responseJSON.error;
-  //       for (var key in errors) {
-  //         $('#' + key + '-error').text(errors[key][0]);
-  //       }
-  //     });
-  // });
+    // Check if color is not saved
+    if (!paragraph.data('previous-color')) {
+      paragraph.data('previous-color', paragraph.css('color'));
+    }
+
+    // Change color to grey if it's not grey, else restore previous color
+    if (paragraph.css('color') === 'rgb(128, 128, 128)') {
+      paragraph.css('color', paragraph.data('previous-color'));
+    } else {
+      paragraph.css('color', 'grey');
+    }
+
+    var icon = $(this).find('ion-icon');
+
+    if (icon.attr('name') === 'pause-circle-outline') {
+      icon.attr('name', 'play-circle-outline');
+    } else {
+      icon.attr('name', 'pause-circle-outline');
+    }
+  });
 });
-
