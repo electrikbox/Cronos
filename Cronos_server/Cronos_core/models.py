478b3a300059012a6703bef1bf284783672273af
ValentinePumpkins <quignon.val@gmail.com>
2024-02-13 10:03:24 +0100

    class Activation temp token

--- Cronos_server/Cronos_core/models.py
+++ Cronos_server/Cronos_core/models.py
@@ -28,6 +28,9 @@ class Logs(models.Model):
     cron = models.ForeignKey('Crons', on_delete=models.CASCADE)
 
 
+# TEMPORARY TOKEN (reset pwd + activation)
+# =============================================================================
+
 class PasswordTemporaryToken(models.Model):
     """ Class for temporary password reset token  """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -50,6 +53,30 @@ class PasswordTemporaryToken(models.Model):
         """ Check if token is valid  """
         return timezone.now() <= self.expires_at
 
+
+class ActivationTemporaryToken(models.Model):
+    """ Class for activation temporary token  """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    activation_token = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    expires_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        """ Generate a token if not exists & set the expiration date """
+        if not self.activation_token:
+            self.activation_token = secrets.token_urlsafe(32)
+
+        if not self.expires_at:
+            self.expires_at = timezone.now() + datetime.timedelta(days=7)  # modif ici le délai
+        super().save(*args, **kwargs)
+
+    def is_valid(self):
+        """ Check if token is valid  """
+        return timezone.now() <= self.expires_at
+
+
+# CRONS
+# =============================================================================
 
 class Crons(models.Model):
     """ Class Crons """
