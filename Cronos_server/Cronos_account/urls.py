ac21702f2d309935b7fbcb90872dd534e59cd5aa
ValentinePumpkins <quignon.val@gmail.com>
2024-02-14 18:09:16 +0100

    rm url change_password (no need)

--- Cronos_server/Cronos_account/urls.py
+++ Cronos_server/Cronos_account/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('signup/activation_error/', views.activation_error, name='activation_error'),
     path('signup/pending/', views.pending_activation, name='pending_activation'),
     path('user_account/', views.user_account, name='user_account'),
-    path('change_password/', views.change_password, name='change_password'),
     path('forget_password/', views.forget_password, name='forget_password'),
     path('forget_password/reset/<uidb64>/<token_temp_key>/', views.reset_password, name='reset_password'),
     path('activate/<uidb64>/<temp_token_key>/', views.activate_account, name='activate_account'),
