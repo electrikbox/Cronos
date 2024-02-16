a8f132748f9773b28ecfa418e0c3060f1206aba8
ValentinePumpkins <quignon.val@gmail.com>
2024-02-16 18:13:30 +0100

    import updated for img forms

--- Cronos_server/Cronos_website/forms/__init__.py
+++ Cronos_server/Cronos_website/forms/__init__.py
@@ -1,6 +1,7 @@
 """ Class CronFom """
 
 from django import forms
+from django.core.validators import FileExtensionValidator
 from Cronos_core.models import Crons
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -9,4 +10,4 @@ from Cronos_website.forms.cron_form import CronForm
 from Cronos_website.forms.forget_password_forms import ForgetPasswordForm, SetNewPasswordForm
 from Cronos_website.forms.login_form import LoginFormCustom
 from Cronos_website.forms.signup_form import SignUpForm
-from Cronos_website.forms.user_account_forms import UserAccountForm, UserAccountPwdForm
+from Cronos_website.forms.user_account_forms import UserAccountForm, UserAccountPwdForm, ProfileImgForm
