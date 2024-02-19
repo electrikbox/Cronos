7a3b09266a687caf45ae742b7b9ba13353a160cb
ValentinePumpkins <quignon.val@gmail.com>
2024-02-19 16:45:11 +0100

    add resize method + del existing img if needed

--- Cronos_server/Cronos_account/user_pics.py
+++ Cronos_server/Cronos_account/user_pics.py
@@ -1,4 +1,5 @@
 from Cronos_core.models import *
+from PIL import Image
 import glob
 
 
@@ -9,23 +10,35 @@ class UserPic():
         """ Initialize user profile picture """
         self.user = user
 
+    # UPLOAD USER PROFILE PICTURE
+    # =========================================================================
+
     def upload_pic(self, pic):
         """ Upload user profile picture """
-
         _, file_extension = os.path.splitext(pic.name)
+
 
         filename = f"{self.user.id}{file_extension.lower()}"
         filepath = os.path.join(settings.MEDIA_ROOT, filename)
 
+        filename_no_ext = f"{self.user.id}"
+        existing_files = glob.glob(f"{settings.MEDIA_ROOT}/{filename_no_ext}.*")
+
+        for existing_file in existing_files:
+            os.remove(existing_file)
+
+        resized_img = self.resize_pic(pic)
+
         with open(filepath, 'wb') as f:
-            for chunk in pic.chunks():
-                f.write(chunk)
+            resized_img.save(f)
 
         return filename
 
+    # SHOW USER PROFILE PICTURE
+    # =========================================================================
+
     def show_pic(self):
         """ Show user profile picture """
-
         filepath = os.path.join(settings.MEDIA_ROOT)
         search_file = f"{filepath}/{self.user.id}".split('.')[0]
 
@@ -44,3 +57,35 @@ class UserPic():
         image_url = settings.MEDIA_URL + pic
 
         return image_url
+
+    # RESIZE USER PROFILE PICTURE
+    # =========================================================================
+
+    @staticmethod
+    def resize_pic(pic, size=200):
+        """ Resize user profile picture """
+        img = Image.open(pic)
+        img_width, img_height = img.size
+        aspect_ratio = img_width / img_height
+
+        # Square size
+        if img_width > img_height:
+            new_width = size
+            new_height = int(size / aspect_ratio)
+        else:
+            new_height = size
+            new_width = int(size * aspect_ratio)
+
+        resized_img = img.resize((new_width, new_height))
+
+        # Create a new image with a transparent background if needed
+        if img.mode in ('RGBA', 'LA'):
+            square_img = Image.new('RGBA', (size, size), (255, 255, 255, 0))
+        else:
+            square_img = Image.new('RGB', (size, size), (255, 255, 255))
+
+        # Paste the resized image into the center of the square image
+        offset = ((size - new_width) // 2, (size - new_height) // 2)
+        square_img.paste(resized_img, offset)
+
+        return square_img
