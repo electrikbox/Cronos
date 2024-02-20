b8bdcb5f0f88cdd67d8ab5f3077cc01f978dc8b5
ValentinePumpkins <quignon.val@gmail.com>
2024-02-20 15:49:54 +0100

    view contact

--- Cronos_server/Cronos_website/views/__init__.py
+++ Cronos_server/Cronos_website/views/__init__.py
@@ -3,10 +3,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from Cronos_API import CRON_CREATE_API_URL, CRON_LIST_API_URL, LOGS_LIST_API_URL
-from Cronos_website.forms import CronForm
+from Cronos_website.forms import CronForm, ContactForm
 from Cronos_account.user_pics import UserPic
-import requests
+from Cronos_core.models import Profiles
 from Cronos_core.models import Logs
+import requests
 
 from Cronos_website.views.landing_page import landing_page
 from Cronos_website.views.downloads import downloads
@@ -14,3 +15,4 @@ from Cronos_website.views.contact import contact
 from Cronos_website.views.FAQ import FAQ
 from Cronos_website.views.home import home
 from Cronos_website.views.dashboard import dashboard
+from Cronos_website.views.contact import contact
--- Cronos_server/Cronos_website/views/contact.py
+++ Cronos_server/Cronos_website/views/contact.py
@@ -1,13 +1,38 @@
+from operator import is_
 from Cronos_website.views import *
 
 
 @login_required
 def contact(request) -> HttpResponse:
     """Render contact page"""
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+    else:
+        user = request.user
+        if user.is_authenticated:
+            try:
+                profile = Profiles.objects.get(user=user)
+                initial_data = {
+                    'name': f"{profile.first_name} {profile.last_name}",
+                    'email': user.email
+                }
+                form = ContactForm(initial=initial_data)
+            except Profiles.DoesNotExist:
+                form = ContactForm()
+        else:
+            form = ContactForm()
+
     user_pic = UserPic(request.user)
     image_url = user_pic.show_pic()
 
     context = {
-        "image_url": image_url
+        "image_url": image_url,
+        "contact_form": form
     }
     return render(request, "contact.html", context)
