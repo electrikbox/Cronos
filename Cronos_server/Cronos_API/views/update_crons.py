d372ac6dc7a6c0e07a564ea659896f585a24b211
electrikbox <electrikbox@icloud.com>
2024-02-21 10:31:35 +0100

    remove commented lines

--- Cronos_server/Cronos_API/views/update_crons.py
+++ Cronos_server/Cronos_API/views/update_crons.py
@@ -1,81 +1,5 @@
 from Cronos_API.views import *
 
-# @api_view(["PUT", "POST"])
-# @authentication_classes([TokenAuthentication, SessionAuthentication])
-# @permission_classes([IsAuthenticated])
-# def update_cron(request, cron_id) -> Response:
-#     """Update a cron by its id"""
-#     cron_instance = get_object_or_404(models.Crons, pk=cron_id)
-#     cron_serializer = CronsSerializer(
-#         cron_instance, data=request.data, partial=True
-#     )
-
-#     if not cron_serializer.is_valid():
-#         return Response(
-#             cron_serializer.errors, status=status.HTTP_400_BAD_REQUEST
-#         )
-
-#     cron_serializer.save(updated_date=timezone.now())
-
-#     log_data = {
-#         "log": f"({cron_instance.id}) Updated",
-#         "create_date": timezone.now(),
-#         "user": cron_instance.user.id if cron_instance.user else None,
-#         "cron": cron_instance.id,
-#     }
-
-#     log_serializer = LogsSerializer(data=log_data)
-
-#     if not log_serializer.is_valid():
-#         return Response(
-#             cron_serializer.errors, status=status.HTTP_400_BAD_REQUEST
-#         )
-
-#     log_serializer.save()
-#     return Response(cron_serializer.data, status=status.HTTP_200_OK)
-
-
-# @api_view(["PUT", "POST"])
-# @authentication_classes([TokenAuthentication, SessionAuthentication])
-# @permission_classes([IsAuthenticated])
-# def pause_multiple_elements(request):
-#     data = request.data
-#     ids_to_delete = data.get("ids", [])
-
-#     for id in ids_to_delete:
-#         cron_instance = get_object_or_404(models.Crons, id=id)
-#         cron_serializer = CronsSerializer(
-#             cron_instance,
-#             data={"is_paused": data.get("is_paused")},
-#             partial=True
-#         )
-
-#         if not cron_serializer.is_valid():
-#             return Response(
-#                 cron_serializer.errors,
-#                 status=status.HTTP_400_BAD_REQUEST
-#         )
-#         cron_serializer.save(updated_date=timezone.now())
-
-#         log_data = {
-#             "log": f"({cron_instance.id}) Paused",
-#             "create_date": timezone.now(),
-#             "user": request.user.id,
-#             "cron": cron_instance.id,
-#         }
-
-#         log_serializer = LogsSerializer(data=log_data)
-
-#         if not log_serializer.is_valid():
-#             print(log_serializer.errors)
-#             return Response(
-#                 log_serializer.errors, status=status.HTTP_400_BAD_REQUEST
-#             )
-
-#         log_serializer.save()
-
-#     return Response(status=status.HTTP_200_OK)
-
 @api_view(["PUT", "POST"])
 @authentication_classes([TokenAuthentication, SessionAuthentication])
 @permission_classes([IsAuthenticated])
@@ -116,7 +40,11 @@ def pause_multiple_elements(request):
             )
         cron_serializer.save(updated_date=timezone.now())
         create_log(cron_instance.id, "Paused", request.user.id)
-    return Response(status=status.HTTP_200_OK)
+
+    return Response(
+        {"message": "All crons updated successfully"},
+        status=status.HTTP_200_OK
+    )
 
 
 def create_log(cron_id, action, user_id):
