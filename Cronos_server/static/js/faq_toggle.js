e5e2a0bbae8889e9f8dd987d07f65d1a5dc3810e
ValentinePumpkins <quignon.val@gmail.com>
2024-02-21 14:04:25 +0100

    add tuto section in js (toggle answer)

--- Cronos_server/static/js/faq_toggle.js
+++ Cronos_server/static/js/faq_toggle.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('.toggle-answer').click(function () {
-    var question = $(this).closest('.question');
+    var question = $(this).closest('.question, .tuto');
     var answer = question.find('.answer');
     answer.slideToggle();
     $(this).toggleClass('open');
