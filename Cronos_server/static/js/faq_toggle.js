08f25bf47e3d3c7ec2ecdd1e259a27c2517e6ca0
ValentinePumpkins <quignon.val@gmail.com>
2024-02-21 11:23:58 +0100

    toggle js for faq part in help_center

---
+++ Cronos_server/static/js/faq_toggle.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('.toggle-answer').click(function () {
+    var question = $(this).closest('.question');
+    var answer = question.find('.answer');
+    answer.slideToggle();
+    $(this).toggleClass('open');
+
+    if ($(this).hasClass('open')) {
+      $(this).html('▽');
+    } else {
+      $(this).html('▶︎');
+    }
+  });
+});
