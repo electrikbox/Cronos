759de09254196babdef101f2fe5deb992f400519
electrikbox <electrikbox@icloud.com>
2024-02-21 13:23:45 +0100

    copy file from FAQ branch

--- Cronos_server/static/js/file_name.js
+++ Cronos_server/static/js/file_name.js
@@ -1,11 +1,16 @@
-$(document).ready(function() {
-  $('#input_profile_img').change(function() {
-      const fileNameDisplay = $('#file-name-display');
+$(document).ready(function () {
+  const inputFile = document.getElementById('input_profile_img');
+  const fileNameDisplay = $('#file-name-display');
 
-      if (this.files.length > 0) {
-          fileNameDisplay.text(this.files[0].name);
+  if (inputFile) {
+    inputFile.addEventListener('change', function () {
+      if (inputFile.files.length > 0) {
+        fileNameDisplay.text(inputFile.files[0].name);
+        fileNameDisplay.addClass('file-selected');
       } else {
-          fileNameDisplay.text('No file selected');
+        fileNameDisplay.text('No file selected');
+        fileNameDisplay.removeClass('file-selected');
       }
-  });
+    });
+  }
 });
