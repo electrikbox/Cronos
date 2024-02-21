36aa97814bdfbb30cb4461cbaab848e79e304407
ValentinePumpkins <quignon.val@gmail.com>
2024-02-21 12:51:04 +0100

    correct error in console

--- Cronos_server/static/js/file_name.js
+++ Cronos_server/static/js/file_name.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
+$(document).ready(function () {
   const inputFile = document.getElementById('input_profile_img');
-  const fileNameDisplay = document.getElementById('file-name-display');
+  const fileNameDisplay = $('#file-name-display');
 
   inputFile.addEventListener('change', function () {
     if (inputFile.files.length > 0) {
-      fileNameDisplay.textContent = inputFile.files[0].name;
-      fileNameDisplay.classList.add('file-selected');
+      fileNameDisplay.text(inputFile.files[0].name);
+      fileNameDisplay.addClass('file-selected');
     } else {
-      fileNameDisplay.textContent = 'No file selected';
-      fileNameDisplay.classList.remove('file-selected');
+      fileNameDisplay.text('No file selected');
+      fileNameDisplay.removeClass('file-selected');
     }
   });
 });
