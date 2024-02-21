9722e89131952b3674d30ddfceb1e6773292ed07
ValentinePumpkins <quignon.val@gmail.com>
2024-02-21 13:15:32 +0100

    correct JS console err in others pages

--- Cronos_server/static/js/file_name.js
+++ Cronos_server/static/js/file_name.js
@@ -2,13 +2,15 @@ $(document).ready(function () {
   const inputFile = document.getElementById('input_profile_img');
   const fileNameDisplay = $('#file-name-display');
 
-  inputFile.addEventListener('change', function () {
-    if (inputFile.files.length > 0) {
-      fileNameDisplay.text(inputFile.files[0].name);
-      fileNameDisplay.addClass('file-selected');
-    } else {
-      fileNameDisplay.text('No file selected');
-      fileNameDisplay.removeClass('file-selected');
-    }
-  });
+  if (inputFile) {
+    inputFile.addEventListener('change', function () {
+      if (inputFile.files.length > 0) {
+        fileNameDisplay.text(inputFile.files[0].name);
+        fileNameDisplay.addClass('file-selected');
+      } else {
+        fileNameDisplay.text('No file selected');
+        fileNameDisplay.removeClass('file-selected');
+      }
+    });
+  }
 });
