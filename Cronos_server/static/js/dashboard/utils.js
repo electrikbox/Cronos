48e31548179a7b76017cfbb7920150febbf3055a
electrikbox <electrikbox@icloud.com>
2024-02-25 18:30:19 +0100

    handle copy field hide

--- Cronos_server/static/js/dashboard/utils.js
+++ Cronos_server/static/js/dashboard/utils.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
 
   $('select option[value="separator"]').prop('disabled', true);
+  $('.copy_field').css('display', 'none');
 
   $('#id_command').change(function () {
     const command = $(this).val();
     if (command !== 'open') {
       $('.url_field').css('display', 'none');
+      $('.copy_field').css('display', 'block');
     } else {
       $('.url_field').css('display', 'block');
+      $('.copy_field').css('display', 'none');
     }
   });
 });
