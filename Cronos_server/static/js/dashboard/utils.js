2b95306d2e5f04caf7579ad5403e788958d9e2f6
electrikbox <electrikbox@icloud.com>
2024-02-27 00:49:44 +0100

    add help texte in create form

--- Cronos_server/static/js/dashboard/utils.js
+++ Cronos_server/static/js/dashboard/utils.js
@@ -16,6 +16,16 @@ $(document).ready(function () {
       $('.url_field').css('display', 'none');
       $('.copy_field').css('display', 'none');
     }
+
+    if (command === 'ls') {
+      $('.help_create_text').text('ℹ️ This command will list all files founded with the name you enter to a txt file inside Cronos folder in your personal directory. you can use \'*\' to select all files name with the given extension (exemple: *.gif).');
+    } else if (command === 'cp') {
+      $('.help_create_text').text('ℹ️ This command will copy all files founded with the name you enter to a folder inside Cronos folder in your personal directory. you can use \'*\' to select all files name with the given extension (exemple: *.gif).');
+    } else if (command === 'open') {
+      $('.help_create_text').text('ℹ️ This command will simply open the given url in your brower.');
+    } else {
+      $('.help_create_text').text('');
+    }
   }
 
   $('#id_command').change(function () {
