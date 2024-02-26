b75ce6140a20cadd753bb178672461362ea56289
electrikbox <electrikbox@icloud.com>
2024-02-26 01:39:02 +0100

    handle field by the selected command on choice and reload

--- Cronos_server/static/js/dashboard/utils.js
+++ Cronos_server/static/js/dashboard/utils.js
@@ -1,16 +1,22 @@
 $(document).ready(function () {
 
   $('select option[value="separator"]').prop('disabled', true);
-  $('.copy_field').css('display', 'none');
+  // $('.copy_field').css('display', 'none');
 
-  $('#id_command').change(function () {
-    const command = $(this).val();
-    if (command !== 'open') {
+  function updateCopyField() {
+    const command = $('#id_command').val();
+    if (command === 'cp') {
       $('.url_field').css('display', 'none');
       $('.copy_field').css('display', 'block');
-    } else {
+    } else if (command === 'open'){
       $('.url_field').css('display', 'block');
       $('.copy_field').css('display', 'none');
     }
+  }
+
+  $('#id_command').change(function () {
+    updateCopyField();
   });
+
+  updateCopyField();
 });
