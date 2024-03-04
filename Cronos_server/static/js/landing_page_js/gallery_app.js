7fe56a63c4839280580b41cd8c8fd0d78625bbf3
ValentinePumpkins <quignon.val@gmail.com>
2024-03-04 12:15:32 +0100

    gallery app carousel

---
+++ Cronos_server/static/js/landing_page_js/gallery_app.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  let slideIndex = 0;
+  let $slides = $('.gallery-app-img');
+
+  carousel();
+
+  function carousel() {
+    $slides.eq(slideIndex).fadeIn(1000);
+
+    setTimeout(function () {
+      $slides.hide();
+      slideIndex++;
+      if (slideIndex >= $slides.length) {
+        slideIndex = 0;
+      }
+      carousel();
+    }, 3000);
+  }
+});
