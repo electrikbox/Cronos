881abbaa9a6ea647cada4fbd99870ee9a5a482d5
electrikbox <electrikbox@icloud.com>
2024-03-06 02:18:32 +0100

    remove unecessary comments

--- Cronos_server/static/js/landing_page_js/scroll_background.js
+++ Cronos_server/static/js/landing_page_js/scroll_background.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
   $(window).scroll(function() {
       var scrollPosition = $(this).scrollTop();
+      var newPosition = - scrollPosition / 5;
 
-      // Calculez la nouvelle position verticale de l'image de fond en fonction du défilement
-      var newPosition = - scrollPosition / 5; // Utilisez un signe négatif pour inverser la direction du déplacement
-
-      // Appliquez la nouvelle position à l'arrière-plan de .main-landing
       $('.main-landing').css('background-position', '0px ' + newPosition + 'px');
   });
 });
