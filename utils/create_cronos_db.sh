a156534c1306600c32db646dffa151d1edcd0254
ValentinePumpkins <quignon.val@gmail.com>
2024-03-19 17:40:45 +0100

    comments on utils files

--- utils/create_cronos_db.sh
+++ utils/create_cronos_db.sh
@@ -2,8 +2,11 @@
 
 MYSQL_CONFIG_FILE="../.my.cnf"
 
+# If DB already exist, skip creation
 if mysql --defaults-file="$MYSQL_CONFIG_FILE" -e "USE cronos"; then
   echo -e "'cronos' db already exist...\n...skipping creation."
+
+# Create DB if not exist
 else
   mysql --defaults-file="$MYSQL_CONFIG_FILE" -e "CREATE DATABASE cronos;"
   echo "'cronos' db created."
