470ac5a0b6cba866d64263d2e372a1373039fe64
ValentinePumpkins <quignon.val@gmail.com>
2024-03-19 15:32:29 +0100

    comments for serializer

--- Cronos_server/Cronos_API/serializers/profile_serializer.py
+++ Cronos_server/Cronos_API/serializers/profile_serializer.py
@@ -1,7 +1,7 @@
 from Cronos_API.serializers import *
 
 class ProfileSerializer(serializers.ModelSerializer):
-    """ Serializer for Users """
+    """ Serializer for Profile """
     class Meta(object):
         model = Profiles
         fields = ["id", "first_name", "last_name"]
